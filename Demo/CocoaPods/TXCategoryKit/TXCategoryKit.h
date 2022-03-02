747a4355f0007cbcb2562713f4b1b5f24e644f09
chenxiaojie <chenjie003@qq.com>
2022-03-02 17:10:31 +0800

    add TXCategory 0.2.1

---
+++ Demo/CocoaPods/TXCategoryKit/TXCategoryKit.h
@@ -0,0 +1,17 @@
+//
+//  CJCategoryKit.h
+//  SPHKProjectDev
+//
+//  Created by chenxiaojie on 2018/07/24.
+//  Copyright (c) 2018年 chenxiaojie. All rights reserved.
+//
+
+#pragma mark - Category
+
+#import "NSString+TXString.h"
+#import "UIImage+loadSPBundle.h"
+#import "UIView+TXFrame.h"
+#import "UIView+TXCorner.h"
+#import "NSString+TXMD5.h"
+#import "UIColor+TXColor.h"
+#import "UIView+TXOtherView.h"
--- Demo/Public/Controller/SecondViewController.m
+++ Demo/Public/Controller/SecondViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SecondViewController.h"
+#import "TXCategoryKit.h"
 
 @interface SecondViewController ()
 
